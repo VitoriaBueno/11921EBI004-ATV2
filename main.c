4a0ce631b8a7d00887191f7fbd28b402d641f0c8
VitoriaBueno <68306219+VitoriaBueno@users.noreply.github.com>
2022-06-13 20:00:12 -0300

    Upload documentos att

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
 #define STM32_RCC_AHB1ENR (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
 #define STM32_GPIOA_MODER (STM32_GPIOA_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOA_PUPDR (STM32_GPIOA_BASE+STM32_GPIO_PUPDR_OFFSET)
-#define STM32_GPIO_IDR (STM32_GPIO_BASE+STM32_GPIO_IDR_OFFSET)
+#define STM32_GPIO_IDR (STM32_GPIOA_BASE+STM32_GPIO_IDR_OFFSET)
 #define STM32_GPIOC_MODER (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOC_OTYPER (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
 #define STM32_GPIOC_PUPDR (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
@@ -24,12 +24,17 @@
 #define STM32_GPIOC_BSRR (STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
 
 #define RCC_AHB1ENR_GPIOCEN (1 << 2)
+#define RCC_AHB1ENR_GPIOAEN (1 << 0)
 #define GPIO_MODER_INPUT (0)
 #define GPIO_MODER_OUTPUT (1)
 #define GPIO_MODER_ALT (2)
 #define GPIO_MODER_ANALOG (3)
-#define GPIO_MODER13_SHIFT (26)
-#define GPIO_MODER13_MASK (3 << GPIO_MODER13_SHIFT)
+#define GPIO_MODER_SHIFT(n) ((n) << 1)
+#define GPIO_MODER_MASK(n) (3 << GPIO_MODER_SHIFT(n))
+#define GPIO_PUPDR_SHIFT(n) ((n) << 1) 
+#define GPIO_PUPDR_MASK(n) (3 << GPIO_PUPDR_SHIFT(n))
+
+#define GPIO_IDR(n) (1 << (n))
 
 #define GPIO_OTYPER_PP (0)
 #define GPIO_OTYPER_OD (1)
@@ -41,12 +46,9 @@
 #define GPIO_PUPDR_PULLUP (1)
 #define GPIO_PUPDR_PULLDOWN (2)
 
-#define GPIO_PUPDR13_SHIFT (26)
-#define GPIO_PUPDR13_MASK (3 << GPIO_PUPDR13_SHIFT)
-
-
 #define GPIO_BSRR_SET(n) (1 << (n))
 #define GPIO_BSRR_RST(n) (1 << (n + 16))
+
 
 static uint32_t led_status;
 static const char fw_version[] = {'V','1','.','0'};
@@ -54,20 +56,38 @@ static const char fw_version[] = {'V','1','.','0'};
 int main(int argc, char *argv[])
 {
 
+/* Vetor para piscagem */
+
+static const uint32_t LED_PISCA [] = {200, 400, 600, 800};
+uint8_t contador = 0; 
+
 uint32_t reg;
 uint32_t *pRCC_AHB1ENR  = (uint32_t *)STM32_RCC_AHB1ENR;
 uint32_t *pGPIOC_MODER  = (uint32_t*)STM32_GPIOC_MODER;
 uint32_t *pGPIOC_OTYPER = (uint32_t*)STM32_GPIOC_OTYPER;
 uint32_t *pGPIOC_PUPDR  = (uint32_t*)STM32_GPIOC_PUPDR;
 uint32_t *pGPIOC_BSRR   = (uint32_t*)STM32_GPIOC_BSRR;
+uint32_t *pGPIOA_MODER  = (uint32_t*)STM32_GPIOA_MODER;
+uint32_t *pGPIOA_PUPDR  = (uint32_t*)STM32_GPIOA_PUPDR;
+uint32_t *pGPIOA_IDR  = (uint32_t*)STM32_GPIO_IDR;
 
 reg  = *pRCC_AHB1ENR;
-reg |= RCC_AHB1ENR_GPIOCEN;
+reg |= (RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOAEN);
 *pRCC_AHB1ENR = reg;
 
+reg = *pGPIOA_MODER;
+reg &= ~(GPIO_MODER_MASK(0));
+reg |= (GPIO_MODER_OUTPUT << GPIO_MODER_SHIFT(0));
+*pGPIOA_MODER = reg;
+
+reg = *pGPIOA_PUPDR;
+reg &= ~(GPIO_PUPDR_MASK(0));
+reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(0));
+*pGPIOA_PUPDR = reg;
+
 reg = *pGPIOC_MODER;
-reg &= ~(GPIO_MODER13_MASK);
-reg |= (GPIO_MODER_OUTPUT << GPIO_MODER13_SHIFT);
+reg &= ~(GPIO_MODER_MASK(13));
+reg |= (GPIO_MODER_OUTPUT << GPIO_MODER_SHIFT(13));
 *pGPIOC_MODER = reg;
 
 reg = *pGPIOC_OTYPER;
@@ -76,19 +96,28 @@ reg |= (GPIO_OTYPER_PP << GPIO_OT13_SHIFT);
 *pGPIOC_OTYPER = reg;
 
 reg = *pGPIOC_PUPDR;
-reg &= ~(GPIO_PUPDR13_MASK);
-reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR13_SHIFT);
+reg &= ~(GPIO_PUPDR_MASK(13));
+reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(13));
 *pGPIOC_PUPDR = reg;
+
+
 
  while(1)
  {
-     uint32_t LED_DELAY = 10;
+    reg = *pGPIOA_IDR;
+    if ( (reg & GPIO_IDR(0)) == 0) //botao pressionado
+    {
+        contador++;
+        if (contador > 3)
+        {
+            contador = 0;
+        }  
+    }
+
      *pGPIOC_BSRR = GPIO_BSRR_SET(13);
-     led_status = 0;
-     for(uint32_t i =0; i < LED_DELAY; i++);
+     for(uint32_t i =0; i < LED_PISCA[contador]; i++);
      *pGPIOC_BSRR = GPIO_BSRR_RST(13);
-     led_status =1;
-     for(uint32_t i =0; i < LED_DELAY; i++);
+     for(uint32_t i =0; i < LED_PISCA[contador]; i++);
 
  }
 
